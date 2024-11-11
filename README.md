9a2001d7e0452a557367e72decd73208c3bccad9
Swastipathak <swasti20pathak@gmail.com>
2024-11-11 16:17:09 +0530

    third commit

---
+++ README.md
@@ -0,0 +1,3 @@
+
+E-Commerce-Product-Recommendation-System
+
--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT ='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirments(file_path: str)->list[str]:
-    '''
-    fuction Returns the list of requirments 
-    '''
-    requirments=[]
+def get_requirments(file_path: str) -> List[str]:
+    requirments = []
     with open(file_path) as file_obj:
         requirments = file_obj.readlines()
-        requirments =[req.replace("\n","")for req in requirments]
-        
+        requirments = [req.replace("\n", "") for req in requirments]
+
         if HYPEN_E_DOT in requirments:
             requirments.remove(HYPEN_E_DOT)
-            
-    return requirments
     
+    return requirments
 
 setup(
-    name ='Ecommerce_ML_Project',
-    version= '0.0.1',
-    author= 'Swasti pathak',
-    packages= find_packages(),
-    install_requires =get_requirments('requirments.txt')
+    name='ecommerce_ml_project',
+    version='0.0.1',
+    author='Swasti Pathak',
+    author_email='swasti20pathak@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments('requirments.txt')  # Ensure this matches your actual requirments file
 )
